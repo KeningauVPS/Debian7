78cc13123482bbc9ee6913b4c73acdcac4670a1b
har1st <31117912+har1st@users.noreply.github.com>
2017-08-18 11:38:46 +0800

    Update and rename trial.sh to user-trial.sh

--- user-trial.sh
+++ user-trial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script auto create trial user SSH
 #yg akan expired setelah 1 hari
-#modified by Abu @LdSeptian
+#modified by har1st
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
@@ -13,16 +13,16 @@ useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
 echo -e "\e[38;1m============================="
-echo -e "\e[35;1m====Informasi SSH account===="
-echo -e "\e[33;1m=[Host: \e[35;1m$IP" 
+echo -e "\e[35;1m======Info Akun SSH/VPN======"
+echo -e "\e[33;1m=[Host: \e[35;1m$IP"
+echo -e "\e[33;1m=[Username: \e[35;1m$Login"
+echo -e "\e[33;1m=[Password: \e[35;1m$Pass"
+echo -e "\e[33;1mEXPIRED   : \e[35;1m$exp"
+echo -e "\e[38;1m======Create by har1st™======"
 echo -e "\e[33;1m=[Port OpenSSH: \e[35;1m22,143"
 echo -e "\e[33;1m=[Port Dropbear: \e[35;1m80,443"
 echo -e "\e[33;1m=[Port Squid: \e[35;1m8080,3128"
 echo -e "\e[33;1m=[Config OpenVPN (TCP 1194): \e[35;1mhttp://$IP:85/client.ovpn"
-echo -e "\e[33;1m=[Username: \e[35;1m$Login "
-echo -e "\e[33;1m=[Password: \e[35;1m$Pass"
-echo -e "\e[38;1m-----------------------------"
-echo -e "\e[33;1mAktif Sampai: \e[35;1m$exp"
 echo -e "\e[38;1m============================="
-echo -e "\e[36;1mMod by @LdSeptian | x-about"
+echo -e "\e[36;1mhar1s™_ssh"
 echo -e "\e[30;1m"
