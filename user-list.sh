ac44c382acb497d9406bb1c7696cdb46d69e4216
har1st <31117912+har1st@users.noreply.github.com>
2017-08-18 17:33:28 +0800

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "\e[37;1m=[-------------------------------]="
-echo -e "\e[34;1m[USERNAME]        \e[34;1m  [EXP DATE]   "
+echo -e "\e[34;1m=[   USERNAME     \e[34;1m EXP DATE]="
 echo -e "\e[37;1m=[-------------------------------]="
 echo -e "\e[31;1m"
 while read expired
@@ -14,7 +14,7 @@ do
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo -e "\e[37;1m=[-------------------------------]="
-echo -e "\e[34;1m=[Jumlah akun: $JUMLAH user]="
+echo -e "\e[37;1m=[ \e[34;1m  Jumlah akun: $JUMLAH user]="
 echo -e "\e[37;1m=[-------------------------------]="
-echo -e "\e[36;1mMod by @LdSeptian | x-about"
+echo -e "\e[36;1mMod by har1st"
 echo -e "\e[30;1m"
