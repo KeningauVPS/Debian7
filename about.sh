605e1d91801dc00d170beb6c95fcab736fa1a682
har1st <31117912+har1st@users.noreply.github.com>
2017-08-18 11:04:25 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "\e[34;1m=[Script Auto Install SSH & OpenVPN v1.0]=" 
+echo -e "\e[37;1m=============================="
+echo -e "\e[34;1m=[For Debian 7 32 bit & 64 bit]="
+echo -e "\e[34;1m=[For VPS with KVM and VMWare virtualization]="
+echo -e "\e[37;1m=============================="
+echo -e "\e[34;1m==[Original script by]=="
+echo -e "\e[37;1m*/LdSeptian"
+echo -e "\e[37;1m*/x-about"
+echo -e "\e[37;1m=============================="
+echo -e "\e[38;1mMod by @LdSeptian | x-about"
+echo -e "\e[37;1m=============================="
+echo -e "\e[36;1m=[https://www.facebook.com/leadergpt]="
+echo -e "\e[36;1m=[http://x--about.blogspot.co.id]="
+echo -e "\e[37;1m=============================="
