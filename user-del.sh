2e4c243f11558cfe6f10a1de08248a195ab1b181
har1st <31117912+har1st@users.noreply.github.com>
2017-08-18 17:37:19 +0800

    Update user-del.sh

--- user-del.sh
+++ user-del.sh
@@ -1,5 +1,19 @@
 # Script hapus user SSH
 
+echo -e "\e[37;1m=[-------------------------------]="
+echo -e "\e[34;1m=[   USERNAME     \e[34;1m EXP DATE]="
+echo -e "\e[37;1m=[-------------------------------]="
+echo -e "\e[31;1m"
+while read expired
+do
+        AKUN="$(echo $expired | cut -d: -f1)"
+        ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+        if [[ $ID -ge 1000 ]]; then
+        printf "%-17s %2s\n" "$AKUN" "$exp"
+        fi
+done < /etc/passwd
+
 read -p "=[Nama user SSH yang akan dihapus : " Nama
 
 userdel -r $Nama
