3da6c20ac822c957f407f3f3ea7480e6aa65a0f9
har1st <31117912+har1st@users.noreply.github.com>
2017-08-19 17:10:36 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,14 +2,19 @@
 #About this script
 
 clear
-echo -e "\e[34;1m=[Script Auto Install SSH & OpenVPN v1.0]=" 
-echo -e "\e[37;1m=============================="
-echo -e "\e[34;1m=[For Debian 7 32 bit & 64 bit]="
-echo -e "\e[34;1m=[For VPS with KVM and VMWare virtualization]="
-echo -e "\e[37;1m=============================="
-echo -e "\e[34;1m==[Original script by]=="
-echo -e "\e[37;1m*/LdSeptian"
-echo -e "\e[37;1m*/x-about"
-echo -e "\e[37;1m=============================="
-echo -e "\e[38;1mMod by har1st"
-echo -e "\e[37;1m=============================="
+echo -e "\e[37;1m+=================================+"
+echo -e "\e[36;1m+        Script Auto Install      +" 
+echo -e "\e[36;1m+         SSH & OpenVPN v1        +" 
+echo -e "\e[37;1m+=================================+"
+echo -e "\e[38;1m+ [For Debian 7 32 bit & 64 bit]  +"
+echo -e "\e[38;1m+ [For VPS with KVM and VMWare ]  +"
+echo -e "\e[37;1m+=================================+"
+echo -e "\e[34;1m+  _                __      _     +" 
+echo -e "\e[34;1m+ | |              /  |    | |    +" 
+echo -e "\e[34;1m+ | |__   __ _ _ __`| | ___| |_   +" 
+echo -e "\e[34;1m+ |  _ \ / _` | '__|| |/ __| __|  +" 
+echo -e "\e[34;1m+ | | | | (_| | |  _| |\__ \ |_   +" 
+echo -e "\e[34;1m+ |_| |_|\__,_|_|  \___/___/\__|  +" 
+echo -e "\e[37;1m+=================================+"                     
+                              
+
