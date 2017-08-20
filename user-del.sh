dc143572a1f3c24ec2f84283275c974675f06109
har1st <31117912+har1st@users.noreply.github.com>
2017-08-20 08:57:09 +0800

    Update user-del.sh

--- user-del.sh
+++ user-del.sh
@@ -1,7 +1,7 @@
 # Script hapus user SSH
 
 echo -e "\e[37;1m[-------------------------------]"
-echo -e "\e[37;1m[ USERNAME      \e[34;1m EXP DATE      ]"
+echo -e "\e[37;1m[ USERNAME"      \e[34;1m EXP DATE       ]"
 echo -e "\e[37;1m[-------------------------------]"
 echo -e "\e[31;1m"
 while read expired
